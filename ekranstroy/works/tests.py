8ec31c1185f57a9139ed893d89371d28cf9a6c86
asp78 <asp78@yandex.ru>
2024-01-11 10:13:15 +0300

    test

---
+++ ekranstroy/works/tests.py
@@ -0,0 +1,22 @@
+from http import HTTPStatus
+from django.test import Client, TestCase
+
+class StaticPagesURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_about_url_exists_at_desired_location(self):
+        """Проверка доступности страниц."""
+        response = self.guest_client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+        response = self.guest_client.get('/o_nas/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_page_shows_correct_content(self):
+        """Проверка контента страниц."""
+        response = self.guest_client.get('/')
+        self.assertContains(response, 'Строительство домов и коттеджей')
+
+        response = self.guest_client.get('/o_nas/')
+        self.assertContains(response, 'это строительная компания')
