a13a335d1e8bc942e926bf7e9b330105efffec23
asp78 <asp78@yandex.ru>
2024-01-11 10:54:47 +0300

    test4

--- ekranstroy/ekranstroy/settings.py
+++ ekranstroy/ekranstroy/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
-        'HOST': os.getenv('DB_HOST', default='db'),
+        'HOST': os.getenv('DB_HOST', default='my_site_docker-db-1'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
