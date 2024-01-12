879295449b7605dbffd92b61dc241fcb4a438c5c
asp78 <asp78@yandex.ru>
2024-01-12 03:29:55 +0300

    deploy

--- ekranstroy/ekranstroy/settings.py
+++ ekranstroy/ekranstroy/settings.py
@@ -34,7 +34,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://*.localhost', 'http://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = [
+    'http://*.localhost',
+    'http://*.127.0.0.1',
+    'http://*.158.160.40.222',
+    'http://*.ekranstroy.ru',
+    ]
 
 # Application definition
 
