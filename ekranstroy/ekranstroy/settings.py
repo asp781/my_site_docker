4ef8284c2d8a8bc2dee44af853246122be425f85
asp78 <asp78@yandex.ru>
2024-01-23 03:42:38 +0300

    23 compose

--- ekranstroy/ekranstroy/settings.py
+++ ekranstroy/ekranstroy/settings.py
@@ -173,5 +173,5 @@ LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# 23-01-24 3:37
+# 23-01-24 3:41
 # docker build -t asp781/ekranstroy:latest .
