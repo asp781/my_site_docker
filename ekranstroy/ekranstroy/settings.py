0da585d13e0d02e668fea2350edf82b254585bf4
asp78 <asp78@yandex.ru>
2024-01-25 01:37:48 +0300

    base2

--- ekranstroy/ekranstroy/settings.py
+++ ekranstroy/ekranstroy/settings.py
@@ -169,6 +169,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'index'
+LOGOUT_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
