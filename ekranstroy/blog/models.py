1c2965a6c332d4138b8b7bf070e7a4c0a8c087ec
asp78 <asp78@yandex.ru>
2024-01-27 09:11:17 +0300

    mail

--- ekranstroy/blog/models.py
+++ ekranstroy/blog/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
-
-
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
