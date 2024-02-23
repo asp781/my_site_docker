53b40f0c5bce90f3e0df5c3e8fbde84f678331a1
asp78 <asp78@yandex.ru>
2024-02-23 23:12:19 +0300

    blog

--- ekranstroy/blog/models.py
+++ ekranstroy/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -26,6 +27,8 @@ class Post(models.Model):
 
     objects = models.Manager() # менеджер, применяемый по умолчанию
     published = PublishedManager() # конкретно-прикладной менеджер
+    tags = TaggableManager()
+
 
     class Meta:
         ordering = ['-publish']
@@ -44,4 +47,19 @@ class Post(models.Model):
                 self.slug
             ]
             )
-    #
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [models.Index(fields=['created']),]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
