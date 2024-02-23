e743964cbcd12e5f80731cc55fa3235928a80a7c
asp78 <asp78@yandex.ru>
2024-02-23 23:19:36 +0300

    blog

--- ekranstroy/blog/views.py
+++ ekranstroy/blog/views.py
@@ -1,19 +1,10 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post, Comment
+from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from .forms import EmailPostForm, CommentForm
-from django.core.mail import send_mail
-from django.views.decorators.http import require_POST
-from taggit.models import Tag
-from django.db.models import Count
 
 
-def post_list(request, tag_slug=None):
+def post_list(request):
     post_list = Post.published.all()
-    tag = None
-    if tag_slug:
-        tag = get_object_or_404(Tag, slug=tag_slug)
-        post_list = post_list.filter(tags__in=[tag])
     # Постраничная разбивка с 3 постами на страницу
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
@@ -30,10 +21,8 @@ def post_list(request, tag_slug=None):
     return render(
         request,
         'blog/post/list.html',
-        {
-            'posts': posts,
-            'tag': tag,
-            })
+        {'posts': posts},
+        )
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
@@ -44,29 +33,15 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day
         )
-
-    # Список активных комментариев к этому посту
-    comments = post.comments.filter(active=True)
-
-    # Форма для комментирования пользователями
-    form = CommentForm()
-
-    # Список схожих постов
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
-
     return render(
         request,
         'blog/post/detail.html',
-        {
-        'post': post,
-        'comments': comments,
-        'form': form,
-        'similar_posts': similar_posts,
-        })
+        {'post': post},
+        )
 
 
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 
 def post_share(request, post_id):
     # Извлечь пост по идентификатору id
@@ -82,8 +57,8 @@ def post_share(request, post_id):
             cd = form.cleaned_data
             # ... отправить электронное письмо
             post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = f"{cd['name']} рекомендует вам прочитать " f"{post.title}"
-            message = f"{post.title} {post_url}\n\n" f"{cd['name']} Комментарий: {cd['comments']}"
+            subject = f"{cd['name']} recommends you read " f"{post.title}"
+            message = f"Read {post.title} at {post_url}\n\n" f"{cd['name']}\'s comments: {cd['comments']}"
             send_mail(subject, message, 'asp78@yandex.ru', [cd['to']], fail_silently=False)
             sent = True
     else:
@@ -92,25 +67,4 @@ def post_share(request, post_id):
         'post': post,
         'form': form,
         'sent': sent,
-        })
-
-
-@require_POST
-def post_comment(request, post_id):
-    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
-    comment = None
-    # Комментарий был отправлен
-    form = CommentForm(data=request.POST)
-    if form.is_valid():
-        # Создать объект класса Comment, не сохраняя его в базе данных
-        comment = form.save(commit=False)
-        # Назначить пост комментарию
-        comment.post = post
-        # Сохранить комментарий в базе данных
-        comment.save()
-    return render(request, 'blog/post/comment.html',
-        {
-        'post': post,
-        'form': form,
-        'comment': comment
         })
