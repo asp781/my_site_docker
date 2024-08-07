10a83f781bdb22d865f5a7f9f9b57ecbfdabc810
asp78 <asp78@yandex.ru>
2024-08-07 16:01:50 +0300

    middleware

--- ekranstroy/account/middleware.py
+++ ekranstroy/account/middleware.py
@@ -5,8 +5,8 @@ from django.shortcuts import get_object_or_404, redirect
 def web8000_ekranstroy_middleware(get_response):
     def middleware(request):
         request.META['HTTP_HOST'] = 'ekranstroy.ru'
+        request.META['wsgi.url_scheme'] = 'https'
         host = request.get_host()
-        print(host + ' 123')
         response = get_response(request)
         return response
     return middleware
