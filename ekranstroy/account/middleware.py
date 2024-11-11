459a6518d91d305c6ac92c3cb8744bb93b2cd1a0
asp78 <asp78@yandex.ru>
2024-11-11 10:42:01 +0300

    False

--- ekranstroy/account/middleware.py
+++ ekranstroy/account/middleware.py
@@ -6,7 +6,7 @@ def web8000_ekranstroy_middleware(get_response):
     def middleware(request):
         request.META['HTTP_HOST'] = 'ekranstroy.ru'
         request.META['wsgi.url_scheme'] = 'https'
-        host = request.get_host()
+        # host = request.get_host()
         response = get_response(request)
         return response
     return middleware
