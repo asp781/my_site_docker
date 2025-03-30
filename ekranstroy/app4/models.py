2cf1cadf5eaac086a7e01fa9c613f78ae60ec7c3
asp78 <asp78@yandex.ru>
2025-03-31 00:07:32 +0300

    null

--- ekranstroy/app4/models.py
+++ ekranstroy/app4/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Record(models.Model):
     name = models.CharField(max_length=200)
-    amount = models.CharField(max_length=200)
-    unit = models.CharField(max_length=200)
+    amount = models.CharField(max_length=200, blank=True, null=True)
+    unit = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.name
