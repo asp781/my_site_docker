253f20f52c439f79a2a3faeca8d2fcbff59a53df
asp78 <asp78@yandex.ru>
2025-04-24 00:39:03 +0300

    chat

--- ekranstroy/chat/consumers.py
+++ ekranstroy/chat/consumers.py
@@ -1,35 +1,52 @@
 # chat/consumers.py
 import json
+# from pprint import pprint
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        # pprint(self.scope)
+
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
 
-        # Join room group
+        # Получаем имя пользователя из scope
+        user = self.scope["user"]
+        self.username = user.username
+
+        # Добавляем пользователя в группу комнаты
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
 
         await self.accept()
 
     async def disconnect(self, close_code):
-        # Leave room group
+        # Удаляем пользователя из группы комнаты
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
-    # Receive message from WebSocket
+    # Получаем сообщение от WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
-        # Send message to room group
+        # Отправляем сообщение в группу комнаты с именем пользователя
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message}
+            self.room_group_name,
+            {
+                "type": "chat.message",
+                "message": message,
+                "username": self.username,
+            },
         )
 
-    # Receive message from room group
+    # Получаем сообщение от группы комнаты
     async def chat_message(self, event):
         message = event["message"]
+        username = event["username"]
 
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message}))
+        # Отправляем сообщение в WebSocket с именем пользователя
+        await self.send(
+            text_data=json.dumps(
+                {"message": message, "username": username}
+            )
+        )
