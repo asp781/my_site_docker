77842c586c380c4f3661a4ae8aba0a17ef366d2d
asp78 <asp78@yandex.ru>
2025-04-25 00:43:26 +0300

    0

--- ekranstroy/chat/consumers.py
+++ ekranstroy/chat/consumers.py
@@ -27,6 +27,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Получаем сообщение от WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
+        print(text_data_json)
         message = text_data_json["message"]
 
         # Отправляем сообщение в группу комнаты с именем пользователя
