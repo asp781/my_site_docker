d88e15583305858ce094fba6f2ce8dcf8df69cc2
asp78 <asp78@yandex.ru>
2025-04-26 15:58:01 +0300

    api

--- ekranstroy/api/urls.py
+++ ekranstroy/api/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path('completed/<int:pk>/', views.CompletedRetrieveUpdateDestroy.as_view(), name='completed-retrieve-update-destroy'),
     path('paid/', views.PaidListCreate.as_view(), name='paid-list-create'),
     path('paid/<int:pk>/', views.PaidRetrieveUpdateDestroy.as_view(), name='paid-retrieve-update-destroy'),
+
+    path('login_api/', views.LoginView.as_view(), name='login_api'),
+
 ]
 
 # https://ekranstroy.ru/api/v1/estimates/ (список смет и создание новых)
